d3b57596aaf9a22800a6a31cffc9295c5dcba69b
M7ammed 7amad <mohammed3asd@gmail.com>
2023-06-07 23:05:53 +0700

    Add Some Files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='refresh_migrations',
+    version='1.0',
+    description='Refresh Django Migrations Folders',
+    author='Mo7ammed 7amad',
+    packages=['refresh_migrations'],
+    install_requires=[],
+)
