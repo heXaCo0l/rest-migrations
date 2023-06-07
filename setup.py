07c04839e72c8be6c19d63918fb8b1528090a546
M7ammed 7amad <mohammed3asd@gmail.com>
2023-06-07 23:07:31 +0700

    Rename

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='refresh_migrations',
+    name='rest_migrations',
     version='1.0',
     description='Refresh Django Migrations Folders',
     author='Mo7ammed 7amad',
-    packages=['refresh_migrations'],
+    packages=['rest_migrations'],
     install_requires=[],
 )
